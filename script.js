f3ae3df13ed336dbe25ec8a59d03c59bb7c01b81
Brenda Azeredo <135758864+brendafazeredo@users.noreply.github.com>
2023-07-01 15:46:21 -0300

    Update of some important stuff.

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const boxes = document.querySelectorAll('.boxes > div');
-    
-    boxes.forEach(function(box, index) {
-      box.addEventListener('click', function() {
-        if (!box.classList.contains('active')) {
-          boxes.forEach(function(otherBox) {
-            otherBox.classList.remove('active');
-          });
-          box.classList.add('active');
-        }
-      });
-  
-      // Open the first box by default
-      if (index === 0) {
+  const boxes = document.querySelectorAll('.boxes > div');
+
+  boxes.forEach(function(box, index) {
+    box.addEventListener('click', function() {
+      if (!box.classList.contains('active')) {
+        boxes.forEach(function(otherBox) {
+          otherBox.classList.remove('active');
+        });
         box.classList.add('active');
       }
     });
+
+    // Open the first box by default
+    if (index === 0) {
+      box.classList.add('active');
+    }
   });
+
+  function downloadPDF() {
+    var fileUrl = "https://drive.google.com/file/d/1kRpCly6VhjneXc3guQXafA_vSvTdrxEv/view?usp=share_link";
+
+    var link = document.createElement("a");
+
+    link.href = fileUrl;
+
+    link.setAttribute("download", "resume.pdf");
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+  }
+
+  const downloadButton = document.querySelector('.download-button');
+  downloadButton.addEventListener('click', downloadPDF);
+});
